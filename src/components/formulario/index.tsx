a0f1646c8bc77df638cc8ecec33c5822ee56169e
Jessica Caroline <jessicarolyne@hotmail.com>
2024-06-18 22:53:13 -0300

    Programação - usando CSS e Props

--- src/components/formulario/index.tsx
+++ src/components/formulario/index.tsx
@@ -1,20 +1,22 @@
 import React from "react";
-import Button from "../button";
-import './style.scss';
+import style from './Formulario.module.scss';
+import Botao from "../button";
 
 class Formulario extends React.Component {
   render() {
     return (
-      <form className="novaTarefa">
-        <div className="inputContainer">
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required/>
         </div>
-        <div className="inputContainer">
+        <div className={style.inputContainer}>
         <label htmlFor="tempo">Tempo</label>
         <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" required/>
         </div>
-        <Button/>
+        <Botao>
+          Meu botão!
+        </Botao>
       </form>
     );
   }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Formulario from '../components/formulario';
 import Lista from '../components/lista';
-import './style.scss';
+import style from './App.module.scss';
 function App() {
   return (
-    <div className="AppStyle">
+    <div className={style.AppStyle}>
       <Formulario/>
       <Lista/>
     </div>
