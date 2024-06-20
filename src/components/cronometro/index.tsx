6f39fd70e4e4ba7d6120aebe2f2a4b7bd7963dce
Jessica Caroline <jessicarolyne@hotmail.com>
2024-06-19 21:16:18 -0300

    Programação - Props

---
+++ src/components/cronometro/index.tsx
@@ -0,0 +1,15 @@
+import Botao from "../button";
+import Relogio from "./relogio";
+import style from "./Cronometro.module.scss";
+
+export default function Cronometro() {
+  return(
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o relógio</p>
+      <div className={style.relogioWrapper}>
+        <Relogio/>
+      </div>
+      <Botao>Começar</Botao>
+    </div>
+  )
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import Formulario from '../components/formulario';
+import Formulario from "../components/formulario";
 import Lista from '../components/lista';
 import style from './App.module.scss';
+import Cronometro from '../components/cronometro';
 function App() {
   return (
     <div className={style.AppStyle}>
       <Formulario/>
       <Lista/>
+      <Cronometro/>
     </div>
   );
 }
