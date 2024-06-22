bc9ad61518415f76019e8ebaee75c4e457968875
Jessica Caroline <jessicarolyne@hotmail.com>
2024-06-22 12:54:14 -0300

    Projeto finalizado

--- src/components/lista/item/index.tsx
+++ src/components/lista/item/index.tsx
@@ -15,8 +15,8 @@ export default function Item(
     selecionaTarefa
   }:  Props) {
     return(
-      <li className={`${style.item} ${selecionado && style.itemSelecionado}`} 
-      onClick={() => selecionaTarefa(
+      <li className={`${style.item} ${selecionado && style.itemSelecionado} ${completado && style.itemCompletado}`} 
+      onClick={() => !completado && selecionaTarefa(
         {
           tarefa,
           tempo,
@@ -27,6 +27,7 @@ export default function Item(
       )}>
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {completado && <span className={style.concluido} aria-label='Tarefa completada'></span>}
     </li>
   )
 }
