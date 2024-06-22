8b0ae3fa01ffbe96f8ec6b6a3c1a9f5c5e02f8e1
Jessica Caroline <jessicarolyne@hotmail.com>
2024-06-22 11:16:02 -0300

    Selecionando um item

--- src/components/lista/item/index.tsx
+++ src/components/lista/item/index.tsx
@@ -1,11 +1,32 @@
 import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss';
+import style from './Item.module.scss';
 
-export default function Item({tarefa, tempo, selecionado, completado, id}:  ITarefa) {
-  return(
-    <li className={style.item}>
-    <h3>{tarefa}</h3>
-    <span>{tempo}</span>
-  </li>
+interface Props extends ITarefa {
+selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+export default function Item(
+  {
+    tarefa, 
+    tempo, 
+    selecionado, 
+    completado, 
+    id, 
+    selecionaTarefa
+  }:  Props) {
+    return(
+      <li className={`${style.item} ${selecionado && style.itemSelecionado}`} 
+      onClick={() => selecionaTarefa(
+        {
+          tarefa,
+          tempo,
+          selecionado,
+          completado,
+          id
+        }
+      )}>
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+    </li>
   )
 }
